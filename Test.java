b6892f967a3580d64f31fcb553de5ec318fd3e1e
vijrs25 <vijrs25@gmail.com>
2026-04-06 19:21:33 +0530

    Adding leetcode solve

--- Test.java
+++ Test.java
@@ -2,8 +2,32 @@
 public class Test {
 
 	public static void main(String[] args) {
-		String s = "THis is India";
-		System.out.println(s.replace(" ", ""));
-		System.out.println(s);
+		
+	String a = "abcd";
+	String b = "pqrssr";
+	
+	String res = mergeOfTwoArrays(a,b);
+	System.out.println(res);
 	}
-}
+	
+	private static String mergeOfTwoArrays(String a, String b) {
+		int minOfab = Math.min(a.length(), b.length());
+		String minString = minOfab == a.length()?a:b;
+		int count = 0;
+		StringBuilder s = new StringBuilder();
+		while(count/2<minOfab);
+		{
+			if(count%2 == 0) {
+				System.out.println("while");
+				s.append(a.charAt(count/2));
+				count++;
+			}
+			else {
+				s.append(a.charAt(count/2));
+				count++;
+			}
+		}
+		
+		s.append(minString.substring(count/2));
+		return s.toString();
+	}}
