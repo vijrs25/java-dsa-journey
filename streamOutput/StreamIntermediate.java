2f4e3c8f01235ed51db12d1858d3dc27c19bd7b1
vijrs25 <vijrs25@gmail.com>
2026-04-15 05:18:32 +0530

    Stream intermediate

--- streamOutput/StreamIntermediate.java
+++ streamOutput/StreamIntermediate.java
@@ -1,6 +1,7 @@
 package streamOutput;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,18 +19,42 @@ public class StreamIntermediate {
 	Skip first 2 elements (skip())
 	*/
 	public static void main(String[] args) {
-		List<String> names = List.of("ram", "shyam", "mohan ","christopher","proffessor","mohan");
+		List<String> names = List.of("ram", "shyam", "mohan","christopher","proffessor","mohan");
 		String[] a =  names.toArray(new String[0]);
 		
 		for (String string : a) {
 			System.out.println(string);
 		}
-		
+		// A
 		System.out.println(Arrays.stream(a)
-									.collect(Collectors.groupingBy(String :: length))
+									.collect(Collectors.summingInt(String :: length))
 				);
+		//B
+		System.out.println(Arrays.stream(a).mapToInt(String::length).sum());
+		System.out.println(Arrays.stream(a).mapToDouble(String::length).average().getAsDouble());
+		System.out.println(Arrays.stream(a).collect(Collectors.averagingInt(String::length)));
+		
+		System.out.println(Arrays.stream(a).mapToInt(str -> str.length()).max());
+		System.out.println(Arrays.stream(a).collect(Collectors.reducing("", (c,b) -> c.length()>b.length()?c:b)));
+		
+		System.out.println(Arrays.stream(a).distinct().toList());
+		
+		System.out.println(Arrays.stream(a).sorted(Comparator.comparing(String::length).reversed()).toList());
+		System.out.println(Arrays.stream(a).sorted(Comparator.comparing(String::length)).toList());
+		
+		System.out.println(Arrays.stream(a).sorted(Comparator.comparing(String::length).reversed()).limit(2)
+				.max(Comparator.comparing(String::length).reversed()));
 		
 		
+		System.out.println(
+			    Arrays.stream(a)
+			          .sorted(Comparator.comparing(String::length).reversed())
+			          .skip(2)
+			          .findFirst()
+			          .orElse("")
+			);
+		
+		System.out.println(Arrays.stream(a).sorted(Comparator.comparing(String::length).reversed()).limit(3).toList());
 	}
 
 }
